7f63d2070872b532c209166a293156ec7ff4b86b
lucienwalewski <lucien.walewski@polytechnique.edu>
2021-09-23 22:56:44 +0200

    Completed bx2tac

--- labs/lab2/bx2tac.py
+++ labs/lab2/bx2tac.py
@@ -1,9 +1,24 @@
 import sys
+import json
 from lexer import lexer
 from parser import parser_yacc
+from syntax_checker import check_syntax
+from ast2tac import Prog
 
 if __name__ == '__main__':
-    file = sys.argv[1]
-    with open(file, 'r') as fp:
-        res = parser_yacc.parse(fp.read(), lexer=lexer)
-        print(res)
+    filename = sys.argv[1]
+    with open(filename, 'r') as fp:
+        try:
+            parse_output = parser_yacc.parse(fp.read(), lexer=lexer)
+        except SyntaxError as serr:
+            exit(1)
+
+        if check_syntax(parse_output, filename):
+            sys.exit(1)
+
+        tac = Prog(parse_output, 'tmm')
+
+        tacname = filename[:-3] + '.tac.json'
+        with open(tacname, 'w') as fp:
+            json.dump(tac.js_obj, fp)
+        print(f'{filename} -> {tacname}')
