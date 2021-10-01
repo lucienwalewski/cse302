b330fd682f2743fe4b0b4d55eed523228cbe8bf7
PUECH Alban <72336171+albanpuech@users.noreply.github.com>
2021-10-01 19:32:10 +0200

    Update tac2x64.py
    fully functional.

--- labs/lab3/tac2x64.py
+++ labs/lab3/tac2x64.py
@@ -1,40 +1,175 @@
-import sys
+#!/usr/bin/env python3
+
+"""
+This is a very simple TAC to x64 assembly pass. It only handles straightline
+code and a single main() function.
+
+Usage: python3 tac2asm.py tacfile.json
+Produces: tacfile.s (assembly) and tacfile.exe (executable)
+Requires: a working gcc
+"""
+
 import json
+import sys
+import os
 
+jcc = {"jz":(lambda arg, label : ['movq $0, %r11',
+                                      f'cmpq %r11, {arg}',
+                                      f'jz {label}']),
+       "jnz":(lambda arg, label : ['movq $0, %r11',
+                                      f'cmpq %r11, {arg}',
+                                      f'jnz {label}']),
+       "jl":(lambda arg, label : ['movq $0, %r11',
+                                      f'cmpq %r11, {arg}',
+                                      f'jl {label}']),
+       "jnl":(lambda arg, label : ['movq $0, %r11',
+                                      f'cmpq %r11, {arg}',
+                                      f'jnl {label}']),
+       "jle":(lambda arg, label : ['movq $0, %r11',
+                                      f'cmpq %r11, {arg}',
+                                      f'jle {label}']),
+       "jnle":(lambda arg, label : ['movq $0, %r11',
+                                      f'cmpq %r11, {arg}',
+                                      f'jnle {label}'])
+       }
+       
+       
+       
+       
+       
+binops = {'add': 'addq',
+          'sub': 'subq',
+          'mul': (lambda ra, rb, rd: [f'movq {ra}, %rax',
+                                      f'imulq {rb}',
+                                      f'movq %rax, {rd}']),
+          'div': (lambda ra, rb, rd: [f'movq {ra}, %rax',
+                                      f'cqto',
+                                      f'idivq {rb}',
+                                      f'movq %rax, {rd}']),
+          'mod': (lambda ra, rb, rd: [f'movq {ra}, %rax',
+                                      f'cqto',
+                                      f'idivq {rb}',
+                                      f'movq %rdx, {rd}']),
+          'and': 'andq',
+          'or': 'orq',
+          'xor': 'xorq',
+          'shl': (lambda ra, rb, rd: [f'movq {ra}, %r11',
+                                      f'movq {rb}, %rcx',
+                                      f'salq %cl, %r11',
+                                      f'movq %r11, {rd}']),
+          'shr': (lambda ra, rb, rd: [f'movq {ra}, %r11',
+                                      f'movq {rb}, %rcx',
+                                      f'sarq %cl, %r11',
+                                      f'movq %r11, {rd}'])}
+unops = {'neg': 'negq',
+         'not': 'notq'}
 
-def tac_to_x64(tac_instrs):
-    for instruc in tac_instrs:
-        pass  # Do something
+def lookup_temp(temp, temp_map):
+  assert (isinstance(temp, str) and \
+          temp[0] == '%' and \
+          temp[1:].isnumeric()), temp
+  return temp_map.setdefault(temp, f'{-8 * (len(temp_map) + 1)}(%rbp)')
 
+def tac_to_asm(tac_instrs):
+  """
+  Get the x64 instructions correspondign to the TAC instructions
+  """
+  temp_map = dict()
+  asm = []
+  for instr in tac_instrs:
+    opcode = instr['opcode']
+    args = instr['args']
+    result = instr['result']
+    if opcode == 'nop':
+      pass
+    elif opcode == 'const':
+      assert len(args) == 1 and isinstance(args[0], int)
+      result = lookup_temp(result, temp_map)
+      asm.append(f'movq ${args[0]}, {result}')
+    elif opcode == 'label':
+        assert len(args) == 1
+        asm.append(f'{args[0][1:]}:')
+    elif opcode == 'jmp':
+        assert len(args) == 1 
+        asm.append(f'jmp {args[0][1:]}')
+    elif opcode in jcc :
+        jump = jcc[opcode]
+        assert len(args)==2
+        arg = lookup_temp(args[0], temp_map)
+        print(args[1][1:])
+        label = args[1][1:]
+        asm.extend(jump(arg, label))
+    elif opcode == 'copy':
+      assert len(args) == 1
+      arg = lookup_temp(args[0], temp_map)
+      result = lookup_temp(result, temp_map)
+      asm.append(f'movq {arg}, %r11')
+      asm.append(f'movq %r11, {result}')
+    elif opcode in binops:
+      assert len(args) == 2
+      arg1 = lookup_temp(args[0], temp_map)
+      arg2 = lookup_temp(args[1], temp_map)
+      result = lookup_temp(result, temp_map)
+      proc = binops[opcode]
+      if isinstance(proc, str):
+        asm.extend([f'movq {arg1}, %r11',
+                    f'{proc} {arg2}, %r11',
+                    f'movq %r11, {result}'])
+      else: asm.extend(proc(arg1, arg2, result))
+    elif opcode in unops:
+      assert len(args) == 1
+      arg = lookup_temp(args[0], temp_map)
+      result = lookup_temp(result, temp_map)
+      proc = unops[opcode]
+      asm.extend([f'movq {arg}, %r11',
+                  f'{proc} %r11',
+                  f'movq %r11, {result}'])
+    elif opcode == 'print':
+      assert len(args) == 1
+      assert result == None
+      arg = lookup_temp(args[0], temp_map)
+      asm.extend(["pushq %rdi",
+                  "pushq %rax",
+                  f'movq {arg}, %rdi',
+                  "callq bx_print_int",
+                  "popq %rax",
+                  "popq %rdi"])
+    else:
+      assert False, f'unknown opcode: {opcode}'
+  asm[:0] = [f'pushq %rbp',
+             f'movq %rsp, %rbp',
+             f'subq ${8 * len(temp_map)}, %rsp'] \
+  #  + [f'// {tmp} in {reg}' for (tmp, reg) in temp_map.items()]
+  asm.extend([f'movq %rbp, %rsp',
+              f'popq %rbp',
+              f'xorq %rax, %rax',
+              f'retq'])
+  return asm
 
 def compile_tac(fname):
-    if fname.endswith('.tac.json'):
-        rname = fname[:-9]
-    elif fname.endswith('.json'):
-        rname = fname[:-5]
-    else:
-        raise ValueError(f'{fname} does not end in .tac.json or .json')
-    tjs = None
-    with open(fname, 'rb') as fp:
-        tjs = json.load(fp)
-    assert isinstance(tjs, list) and len(tjs) == 1, tjs
-    tjs = tjs[0]
-    assert 'proc' in tjs and tjs['proc'] == '@main', tjs
-    asm = ['\t' + line for line in tac_to_x64(tjs['body'])]
-    asm[:0] = [f'\t.globl main',
-               f'\t.text',
-               'main:',
-               r'\tpushq \%rbp',
-               r'\tmovq \%rsp, \%rbp',
-               r'\tsubq $64, \%rsp', # To be modified (currently for 8 temporaries)
-               r'\tmovq \%rbp, \%rsp',
-               r'\tpopq \%rbp',
-               r'\tmovq \$0, \%rax',
-               r'\tretq']
+  assert fname.endswith('.tac')
+  tjs = None
+  with open(fname, 'rb') as fp:
+    tjs = json.load(fp)
+  assert isinstance(tjs, list) and len(tjs) == 1, tjs
+  tjs = tjs[0]
+  assert 'proc' in tjs and tjs['proc'] == '@main', tjs
+  asm = ['\t' + line for line in tac_to_asm(tjs['body'])]
+  asm[:0] = [f'\t.section .rodata',
+             f'.lprintfmt:',
+             f'\t.string "%ld\\n"',
+             f'\t.text',
+             f'\t.globl main',
+             f'main:']
+  xname = fname[:-5] + '.exe'
+  sname = fname[:-5] + '.s'
+  with open(sname, 'w') as afp:
+    print(*asm, file=afp, sep='\n')
+  print(f'{fname} -> {sname}')
 
 
 if __name__ == '__main__':
-    if len(sys.argv) != 2:
-        print(f'Usage: {sys.argv[0]} tacfile.tac.json')
-        sys.exit(1)
-    compile_tac(sys.argv[1])
+  if len(sys.argv) != 2:
+    print(f'Usage: {sys.argv[0]} tacfile.json')
+    sys.exit(1)
+  compile_tac(sys.argv[1])
