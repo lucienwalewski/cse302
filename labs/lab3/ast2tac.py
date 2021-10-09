edd0890f9ee1cfa37cc329271049c301233eef89
lucienwalewski <lucien.walewski@polytechnique.edu>
2021-10-09 21:36:05 +0200

    Implemented typed munch

--- labs/lab3/ast2tac.py
+++ labs/lab3/ast2tac.py
@@ -1,8 +1,15 @@
-#!/usr/bin/env python3
+from bx_ast import *
+from collections import deque
 
-# import tac2asm
-import json
-import bx_ast as ast
+opcode_map = {
+    'PLUS': 'add', 'MINUS': 'sub', 'TIMES': 'mul', 'DIV': 'div',
+    'MODULUS': 'mod', 'BITAND': 'and', 'BITOR': 'or', 'BITXOR': 'xor',
+    'BITSHL': 'shl', 'BITSHR': 'shr', 'BITCOMPL': 'not',
+    'UMINUS': 'neg',
+
+    'EQUALITY': 'jz', 'DISSEQUALITY': 'jnz', 'LT': 'jnge', 'LEQ': 'jng',
+    'GT': 'jnle', 'GEQ': 'gnl'
+}
 
 ####################
 # Instructions
@@ -23,32 +30,20 @@ class Instr:
                 'args': self.args,
                 'result': self.result}
 
-####################
-# Procedures
 
-
-opcode_map = {
-    'PLUS': 'add', 'MINUS': 'sub', 'TIMES': 'mul', 'DIV': 'div',
-    'MODULUS': 'mod', 'BITAND': 'and', 'BITOR': 'or', 'BITXOR': 'xor',
-    'BITSHL': 'shl', 'BITSHR': 'shr', 'BITCOMPL': 'not',
-    'UMINUS': 'neg'
-}
-
-
-class Prog:
-    def __init__(self, ast_prog: ast.Program, alg):
+class Prog():
+    def __init__(self, ast_prog: Program, alg):
         self.localtemps = []
         self.instrs = []
         self.__tempmap = dict()
         self.__last = -1
+        self.__last_label = -1
+        self._break_stack = deque()
+        self._continue_stack = deque()
         for v in ast_prog.lvars:
             self._emit('const', [0], self._lookup(v))
-        if alg == 'tmm':
-            for stmt in ast_prog.stmts:
-                self.tmm_stmt(stmt)
-        else:
-            for stmt in ast_prog.stmts:
-                self.bmm_stmt(stmt)
+        for stmt in ast_prog.stmts:
+            self.tmm_stmt(stmt)
 
     @property
     def js_obj(self):
@@ -61,6 +56,11 @@ class Prog:
         self.localtemps.append(t)
         return t
 
+    def _fresh_label(self):
+        self.__last_label += 1
+        t = f'L{self.__last_label}'
+        return t
+
     def _lookup(self, var):
         t = self.__tempmap.get(var)
         if t is None:
@@ -71,95 +71,248 @@ class Prog:
     def _emit(self, opcode, args, result):
         self.instrs.append(Instr(opcode, args, result))
 
-    def tmm_expr(self, expr, target):
-        if isinstance(expr, ast.Variable):
+    def tmm_int_expr(self, expr: Expr, target):
+        if isinstance(expr, Number):
+            self._emit('const', [expr.value], target)
+        elif isinstance(expr, Variable):
             src = self._lookup(expr.name)
             self._emit('copy', [src], target)
-        elif isinstance(expr, ast.Number):
-            self._emit('const', [expr.value], target)
-        elif isinstance(expr, ast.OpApp):
+        elif isinstance(expr, OpApp):
             args = []
-            for arg_expr in expr.args:
+            for arg in expr.args:
                 arg_target = self._fresh()
-                self.tmm_expr(arg_expr, arg_target)
+                self.tmm_int_expr(arg, arg_target)
                 args.append(arg_target)
             self._emit(opcode_map[expr.op], args, target)
         else:
-            raise ValueError(f'tmm_expr: unknown expr kind: {expr.__class__}')
+            raise ValueError(
+                f'tmm_expr: unknown expr kind {expr.__class__}')
 
-    def bmm_expr(self, expr):
-        if isinstance(expr, ast.Variable):
-            return self._lookup(expr.name)
-        elif isinstance(expr, ast.Number):
-            target = self._fresh()
-            self._emit('const', [expr.value], target)
-            return target
-        elif isinstance(expr, ast.OpApp):
-            args = [self.bmm_expr(arg) for arg in expr.args]
-            target = self._fresh()
-            self._emit(opcode_map[expr.op], args, target)
-            return target
+    def tmm_bool_expr(self, bexpr: Expr, Lt, Lf):
+        '''Emit code to evaluate 'bexpr', 
+        jumping to 'Lt' if true and 'Lf'
+        if false.'''
+        if isinstance(bexpr, Bool):
+            if bexpr.value == True:
+                self._emit('jmp', [Lt], None)
+            elif bexpr.value == False:
+                self._emit('jmp', [Lf], None)
+        elif isinstance(bexpr, OpApp):
+            if bexpr.op in {'EQUALITY', 'DISEQUALITY',
+                            'LT', 'LEQ' 'GT', 'GEQ'}:
+                args = []
+                for arg in bexpr.args:
+                    arg_target = self._fresh()
+                    self.tmm_int_expr(arg, arg_target)
+                    args.append(arg_target)
+                self._emit('sub', args, args[0])
+                self._emit(opcode_map[bexpr.op], [args[0], Lt], None)
+                self._emit('jmp', [Lf], None)
+            elif bexpr.op in {'BOOLAND', 'BOOLOR'}:
+                self.tmm_bool_expr(bexpr.args[0], Lt, Lf)
+                self._emit('label', [self._fresh_label()], None)
+                self.tmm_bool_expr(bexpr.args[1], Lt, Lf)
+            elif bexpr.op == 'BOOLNEG':
+                self.tmm_bool_expr(bexpr.args[0], Lf, Lt)
         else:
-            raise ValueError(f'bmm_expr: unknown expr kind: {expr.__class__}')
+            raise ValueError(
+                f'tmm_expr: unknown expr kind: {bexpr.__class__}')
 
-    def tmm_stmt(self, stmt):
-        if isinstance(stmt, ast.Assign) or isinstance(stmt, ast.Vardecl):
-            t_lhs = self._lookup(stmt.lhs.name)
-            self.tmm_expr(stmt.rhs, t_lhs)
-        elif isinstance(stmt, ast.Print):
-            t = self._fresh()
-            self.tmm_expr(stmt.arg, t)
-            self._emit('print', [t], None)
+    def tmm_stmt(self, stmt: Stmt):
+        if isinstance(stmt, Assign) or isinstance(stmt, Vardecl):
+            target = self._lookup(stmt.var.name)
+            self.tmm_int_expr(stmt.expr, target)
+        elif isinstance(stmt, Print):
+            target = self._fresh()
+            self.tmm_int_expr(stmt.expr, target)
+            self._emit('print', [target], None)
+        elif isinstance(stmt, IfElse):
+            Lt, Lf, Lo = [self._fresh_label() for _ in range(3)]
+            self._emit('label', [Lt], None)
+            self.tmm_stmt(stmt.block)
+            self._emit('jmp', [Lo], None)
+            self._emit('label', [Lf], None)
+            self.tmm_stmt(stmt.ifrest)
+            self._emit('label', [Lo], None)
+        elif isinstance(stmt, Block):
+            for stmt in stmt.stmts:
+                self.tmm_stmt(stmt)
+        elif isinstance(stmt, While):
+            Lhead, Lbod, Lend = [self._fresh_label() for _ in range(3)]
+            self._break_stack.append(Lend)
+            self._continue_stack.append(Lhead)
+            self._emit('label', [Lhead], None)
+            self.tmm_bool_expr(stmt.condition, Lbod, Lend)
+            self._emit('label', [Lbod], None)
+            self.tmm_stmt(stmt.block)
+            self._emit('jmp', [Lhead], None)
+            self._emit('label', [Lend], None)
+            self._break_stack.pop()
+            self._continue_stack.pop()
+        elif isinstance(stmt, Jump):
+            if stmt.op == 'break':
+                self._emit('jmp', [self._break_stack[-1]], None)
+            elif stmt.op == 'continue':
+                self._emit('jmp', [self._continue_stack[-1]], None)
         else:
             raise ValueError(f'tmm_stmt: unknown stmt kind: {stmt.__class__}')
 
-    def bmm_stmt(self, stmt):
-        if isinstance(stmt, ast.Assign):
-            t_lhs = self._lookup(stmt.lhs.name)
-            t_rhs = self.bmm_expr(stmt.rhs)
-            self._emit('copy', [t_rhs], t_lhs)
-        elif isinstance(stmt, ast.Print):
-            t = self.bmm_expr(stmt.arg)
-            self._emit('print', [t], None)
-        else:
-            raise ValueError(f'bmm_stmt: unknown stmt kind: {stmt.__class__}')
 
-####################
-# Driver
+def ast2tac(ast):
+    assert ast['tag'] == 'Program'
+    tac = []
+    _break_stack = []
+    _continue_stack = []
+
+    for stmt in ast['stmts']:
+        if stmt['tag'] == 'Variable':
+            tac.append({})
 
 
-def ast_to_tac_json(fname, alg):
-    assert fname.endswith('.json')
-    with open(fname, 'rb') as fp:
-        js_obj = json.load(fp)
-        ast_prog = ast.Program.load(js_obj['ast'])
-    tac_prog = Prog(ast_prog, alg)
-    tacname = fname[:-4] + 'tac.json'
-    with open(tacname, 'w') as fp:
-        json.dump(tac_prog.js_obj, fp)
-    print(f'{fname} -> {tacname}')
-    return tacname
+# #!/usr/bin/env python3
+
+# # import tac2asm
+# import json
+# import bx_ast as ast
 
 
-def main():
-    import sys
-    import argparse
-    ap = argparse.ArgumentParser(description='Codegen: AST(JSON) to TAC(JSON)')
-    ap.add_argument('--bmm', dest='bmm', action='store_true', default=False,
-                    help='Do bottom-up maximal munch (incompatible with --tmm)')
-    ap.add_argument('--tmm', dest='tmm', action='store_true', default=False,
-                    help='Do top-down maximal munch (incompatible with --bmm)')
-    ap.add_argument('--continue', dest='keepgoing', action='store_true', default=False,
-                    help='Continue with later stages of the compilation chain')
-    ap.add_argument('fname', metavar='FILE', type=str, nargs=1,
-                    help='The AST(JSON) file to process')
-    opts = ap.parse_args(sys.argv[1:])
-    if opts.tmm and opts.bmm:
-        raise ValueError('Cannot use both --tmm and --bmm')
-    alg = 'tmm' if opts.tmm or not opts.bmm else 'bmm'
-    tacname = ast_to_tac_json(opts.fname[0], alg)
-    # if opts.keepgoing:
-    #     tac2asm.compile_tac(tacname)
+# ####################
+# # Procedures
 
-if __name__ == '__main__':
-    main()
+
+# opcode_map = {
+#     'PLUS': 'add', 'MINUS': 'sub', 'TIMES': 'mul', 'DIV': 'div',
+#     'MODULUS': 'mod', 'BITAND': 'and', 'BITOR': 'or', 'BITXOR': 'xor',
+#     'BITSHL': 'shl', 'BITSHR': 'shr', 'BITCOMPL': 'not',
+#     'UMINUS': 'neg'
+# }
+
+
+# class Prog:
+#     def __init__(self, ast_prog: ast.Program, alg):
+#         self.localtemps = []
+#         self.instrs = []
+#         self.__tempmap = dict()
+#         self.__last = -1
+#         for v in ast_prog.lvars:
+#             self._emit('const', [0], self._lookup(v))
+#         if alg == 'tmm':
+#             for stmt in ast_prog.stmts:
+#                 self.tmm_stmt(stmt)
+#         else:
+#             for stmt in ast_prog.stmts:
+#                 self.bmm_stmt(stmt)
+
+#     @property
+#     def js_obj(self):
+#         return [{'proc': '@main',
+#                  'body': [i.js_obj for i in self.instrs]}]
+
+#     def _fresh(self):
+#         self.__last += 1
+#         t = f'%{self.__last}'
+#         self.localtemps.append(t)
+#         return t
+
+#     def _lookup(self, var):
+#         t = self.__tempmap.get(var)
+#         if t is None:
+#             t = self._fresh()
+#             self.__tempmap[var] = t
+#         return t
+
+#     def _emit(self, opcode, args, result):
+#         self.instrs.append(Instr(opcode, args, result))
+
+#     def tmm_expr(self, expr, target):
+#         if isinstance(expr, ast.Variable):
+#             src = self._lookup(expr.name)
+#             self._emit('copy', [src], target)
+#         elif isinstance(expr, ast.Number):
+#             self._emit('const', [expr.value], target)
+#         elif isinstance(expr, ast.OpApp):
+#             args = []
+#             for arg_expr in expr.args:
+#                 arg_target = self._fresh()
+#                 self.tmm_expr(arg_expr, arg_target)
+#                 args.append(arg_target)
+#             self._emit(opcode_map[expr.op], args, target)
+#         else:
+#             raise ValueError(f'tmm_expr: unknown expr kind: {expr.__class__}')
+
+#     def bmm_expr(self, expr):
+#         if isinstance(expr, ast.Variable):
+#             return self._lookup(expr.name)
+#         elif isinstance(expr, ast.Number):
+#             target = self._fresh()
+#             self._emit('const', [expr.value], target)
+#             return target
+#         elif isinstance(expr, ast.OpApp):
+#             args = [self.bmm_expr(arg) for arg in expr.args]
+#             target = self._fresh()
+#             self._emit(opcode_map[expr.op], args, target)
+#             return target
+#         else:
+#             raise ValueError(f'bmm_expr: unknown expr kind: {expr.__class__}')
+
+#     def tmm_stmt(self, stmt):
+#         if isinstance(stmt, ast.Assign) or isinstance(stmt, ast.Vardecl):
+#             t_lhs = self._lookup(stmt.lhs.name)
+#             self.tmm_expr(stmt.rhs, t_lhs)
+#         elif isinstance(stmt, ast.Print):
+#             t = self._fresh()
+#             self.tmm_expr(stmt.arg, t)
+#             self._emit('print', [t], None)
+#         else:
+#             raise ValueError(f'tmm_stmt: unknown stmt kind: {stmt.__class__}')
+
+#     # def bmm_stmt(self, stmt):
+#     #     if isinstance(stmt, ast.Assign):
+#     #         t_lhs = self._lookup(stmt.lhs.name)
+#     #         t_rhs = self.bmm_expr(stmt.rhs)
+#     #         self._emit('copy', [t_rhs], t_lhs)
+#     #     elif isinstance(stmt, ast.Print):
+#     #         t = self.bmm_expr(stmt.arg)
+#     #         self._emit('print', [t], None)
+#     #     else:
+#     #         raise ValueError(f'bmm_stmt: unknown stmt kind: {stmt.__class__}')
+
+# ####################
+# # Driver
+
+
+# def ast_to_tac_json(fname, alg):
+#     assert fname.endswith('.json')
+#     with open(fname, 'rb') as fp:
+#         js_obj = json.load(fp)
+#         ast_prog = ast.Program.load(js_obj['ast'])
+#     tac_prog = Prog(ast_prog, alg)
+#     tacname = fname[:-4] + 'tac.json'
+#     with open(tacname, 'w') as fp:
+#         json.dump(tac_prog.js_obj, fp)
+#     print(f'{fname} -> {tacname}')
+#     return tacname
+
+
+# def main():
+#     import sys
+#     import argparse
+#     ap = argparse.ArgumentParser(description='Codegen: AST(JSON) to TAC(JSON)')
+#     ap.add_argument('--bmm', dest='bmm', action='store_true', default=False,
+#                     help='Do bottom-up maximal munch (incompatible with --tmm)')
+#     ap.add_argument('--tmm', dest='tmm', action='store_true', default=False,
+#                     help='Do top-down maximal munch (incompatible with --bmm)')
+#     ap.add_argument('--continue', dest='keepgoing', action='store_true', default=False,
+#                     help='Continue with later stages of the compilation chain')
+#     ap.add_argument('fname', metavar='FILE', type=str, nargs=1,
+#                     help='The AST(JSON) file to process')
+#     opts = ap.parse_args(sys.argv[1:])
+#     if opts.tmm and opts.bmm:
+#         raise ValueError('Cannot use both --tmm and --bmm')
+#     alg = 'tmm' if opts.tmm or not opts.bmm else 'bmm'
+#     tacname = ast_to_tac_json(opts.fname[0], alg)
+#     # if opts.keepgoing:
+#     #     tac2asm.compile_tac(tacname)
+
+# if __name__ == '__main__':
+#     main()
