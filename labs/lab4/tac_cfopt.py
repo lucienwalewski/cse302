24f28278cf56c1267778baf7bd672cdce751b70e
PUECH Alban <72336171+albanpuech@users.noreply.github.com>
2021-10-14 13:28:30 +0200

    Update tac_cfopt.py

--- labs/lab4/tac_cfopt.py
+++ labs/lab4/tac_cfopt.py
@@ -9,11 +9,56 @@ CFG inference from linearized TAC
 
 
 import sys
+import argparse
 import json
+
+conditional_jumps = [] ## list of cond jump instructions
 
 
 def build_basic_blocks(body):
-    pass
+"""
+1. Add an entry label before first instruction if needed.
+2. For jumps, add a label after the instruction if one doesn’t already exist.
+3. Start a new block at each label; accumulate instructions in the block until encountering a jump
+(inclusive), a ret (inclusive), or another label (exclusive).
+4. Add explicit jmps for fall-throughs. All blocks must end with a ret or a jmp.
+"""
+    list_of_blocks = []
+    block = []
+
+    
+        ## add entry label to block
+        
+    
+    while body != []: 
+
+        i = 0
+        if body[0]["opcode"] != label : ## add label to beginning of the block
+        
+        while True :
+            if body[i]["opcode"] in conditional_jumps and (body[i+1]["opcode"] not in conditional_jumps or i+1 >= len(body)) :
+                break
+            i +=1 
+
+
+        while i<len(body) and body[i]["opcode"] not in ["jmp","ret"] : ## add other jumps to the list for the code to be correct
+            i +=1
+
+        if body[i]["opcode"] in conditional_jumps :
+            ## go to the end of the conditionnal jump sequence and add an unconditionnal jump
+            ## dont forget to increment i when doing this 
+
+        block = body[:i+1]
+        list_of_blocks.append(block)
+        body = body[i+1:]
+
+            
+
+
+            
+
+
+
 
 def build_cfg(basic_blocks):
     pass
@@ -35,27 +80,53 @@ def optimize(json_tac) :
 
 
 if __name__ == '__main__':
-    if sys.argv[1]=="o" :
-        assert argc==4 
-        assert argv[3].endswith(".tac.json")
-        json_tac_file = open(argv[2])
-        json_tac = json.load(json_tac_file)
+    ap = argparse.ArgumentParser(description='Codegen: Tac (JSON) to Tac optimized (JSON)')
+    ap.add_argument('-o', dest='o', action='store_true', default=False,
+                    help="stores resulting json in a file")
+    ap.add_argument('fname', metavar='FILE', type=str, nargs=1,
+                    help='The TAC(JSON) file to process')
+    opts, rem_args = ap.parse_known_args(sys.argv[1:])
+    if opts.o : 
+        ap.add_argument('fname_dest', metavar='FILE_DEST', type=str, nargs=1,
+                    help='The TAC(JSON) file to create')
+        opts = ap.parse_args(sys.argv[1:])
+
+
+    if opts.o :
+        json_tac_file = open(opts.fname[0])
+        json_tac = json.load(json_tac_file)[0]
         optimized_tac = optimize(json_tac)
-        json_tac_file = open(argv[3],"w")
+        json_tac_file = open(opts.fname_dest[0],"w")
         json.dump(optimized_tac,json_tac_file)
+
     else :
-        assert argc==2 
-        assert argv[2].endswith(".tac.json")
-        json_tac_file = open(argv[2])
-        json_tac = json.load(json_tac_file)
+
+        json_tac_file = open(opts.fname[0])
+        json_tac = json.load(json_tac_file)[0]
         optimized_tac = optimize(json_tac)
         print(optimized_tac)
 
+
+    # if sys.argv[1]=="o" :
+    #     assert argc==4 
+    #     assert argv[3].endswith(".tac.json")
+    #     json_tac_file = open(argv[2])
+    #     json_tac = json.load(json_tac_file)
+    #     optimized_tac = optimize(json_tac)
+    #     json_tac_file = open(argv[3],"w")
+    #     json.dump(optimized_tac,json_tac_file)
+    # else :
+    #     assert argc==2 
+    #     assert argv[2].endswith(".tac.json")
+    #     json_tac_file = open(argv[2])
+    #     json_tac = json.load(json_tac_file)
+    #     optimized_tac = optimize(json_tac)
+    #     print(optimized_tac)
+
         
 
         
 
 
     
-    
-1
+
