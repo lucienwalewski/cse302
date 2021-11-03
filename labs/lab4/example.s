51aed9a90b1abc149252c123b1cd0e3c73ab15e2
albanpuech <alban.puech@polytechnique.edu>
2021-11-03 20:10:14 +0100

    updated tac2x64

--- labs/lab4/example.s
+++ labs/lab4/example.s
@@ -1,654 +1,164 @@
-	.section .rodata
-.lprintfmt:
-	.string "%ld\n"
-	.text
+	.globl x
+	.globl y
+	.globl fizzbuzz
+	.globl z
 	.globl main
-main:
+.data
+x:  .quad 10
+y:  .quad 15
+z:  .quad 1
+.text
+fizzbuzz :
 	pushq %rbp
 	movq %rsp, %rbp
-	subq $24, %rsp
-	jmp .L20
-	.L20:
-	jmp .L19
-	.L19:
-	jmp .L18
-	.L21:
-	jmp .L18
-	.L18:
-	jmp .L23
-	.L23:
-	jmp .L17
-	.L22:
-	jmp .L17
-	.L17:
-	jmp .L16
-	.L16:
-	jmp .L29
-	.L30:
-	jmp .L29
-	.L29:
-	jmp .L28
-	.L31:
-	jmp .L24
-	.L28:
-	jmp .L27
-	.L27:
-	jmp .L24
-	.L26:
-	jmp .L24
-	.L25:
-	jmp .L14
-	.L24:
-	jmp .L32
-	.L33:
-	jmp .L14
-	.L32:
-	jmp .L14
-	.L15:
-	jmp .L43
-	.L43:
-	jmp .L41
-	.L42:
-	jmp .L41
-	.L41:
-	jmp .L36
-	.L46:
-	jmp .L36
-	.L45:
-	jmp .L36
-	.L44:
-	jmp .L36
-	.L40:
-	jmp .L47
-	.L47:
-	jmp .L36
-	.L39:
-	jmp .L50
-	.L50:
-	jmp .L38
-	.L49:
-	jmp .L38
-	.L48:
-	jmp .L38
-	.L38:
-	jmp .L36
-	.L51:
-	jmp .L37
-	.L37:
-	jmp .L52
-	.L52:
-	jmp .L0
-	.L36:
-	jmp .L35
-	.L54:
-	jmp .L53
-	.L53:
-	jmp .L35
-	.L35:
-	jmp .L57
-	.L57:
-	jmp .L56
-	.L56:
-	jmp .L34
-	.L55:
-	jmp .L0
-	.L58:
-	jmp .L34
-	.L34:
-	jmp .L66
-	.L66:
-	jmp .L14
-	.L65:
-	jmp .L64
-	.L64:
-	jmp .L67
-	.L67:
-	jmp .L14
-	.L63:
-	jmp .L68
-	.L70:
-	jmp .L69
-	.L69:
-	jmp .L68
-	.L68:
-	jmp .L72
-	.L72:
-	jmp .L14
-	.L71:
-	jmp .L62
-	.L73:
-	jmp .L14
-	.L62:
-	jmp .L74
-	.L75:
-	jmp .L61
-	.L74:
-	jmp .L76
-	.L76:
-	jmp .L61
-	.L61:
-	jmp .L78
-	.L78:
-	jmp .L77
-	.L77:
-	jmp .L60
-	.L60:
-	jmp .L80
-	.L80:
-	jmp .L79
-	.L79:
-	jmp .L59
-	.L81:
-	jmp .L59
-	.L59:
-	jmp .L85
-	.L85:
-	jmp .L84
-	.L84:
-	jmp .L82
-	.L83:
-	jmp .L82
-	.L82:
-	jmp .L86
-	.L87:
-	jmp .L86
-	.L86:
-	jmp .L14
-	.L88:
-	jmp .L0
-	.L14:
-	jmp .L90
-	.L91:
-	jmp .L89
-	.L90:
-	jmp .L13
-	.L92:
-	jmp .L95
-	.L95:
-	jmp .L94
-	.L94:
-	jmp .L89
-	.L93:
-	jmp .L89
-	.L89:
-	jmp .L97
-	.L97:
-	jmp .L96
-	.L96:
-	jmp .L0
-	.L13:
-	jmp .L98
-	.L98:
-	jmp .L12
-	.L12:
-	jmp .L11
-	.L100:
-	jmp .L11
-	.L99:
-	jmp .L11
-	.L11:
-	jmp .L0
-	.L101:
-	jmp .L10
-	.L10:
-	jmp .L0
-	.L102:
-	jmp .L9
-	.L9:
-	jmp .L8
-	.L105:
-	jmp .L8
-	.L104:
-	jmp .L8
-	.L103:
-	jmp .L8
-	.L8:
-	jmp .L107
-	.L107:
-	jmp .L7
-	.L106:
-	jmp .L110
-	.L110:
-	jmp .L7
-	.L109:
-	jmp .L7
-	.L108:
-	jmp .L7
-	.L7:
-	jmp .L120
-	.L121:
-	jmp .L120
-	.L120:
-	jmp .L6
-	.L122:
-	jmp .L6
-	.L119:
-	jmp .L6
-	.L123:
-	jmp .L6
-	.L118:
-	jmp .L126
-	.L126:
-	jmp .L125
-	.L125:
-	jmp .L127
-	.L127:
-	jmp .L124
-	.L124:
-	jmp .L117
-	.L117:
-	jmp .L129
-	.L131:
-	jmp .L129
-	.L130:
-	jmp .L116
-	.L129:
-	jmp .L128
-	.L132:
-	jmp .L116
-	.L128:
-	jmp .L6
-	.L133:
-	jmp .L116
-	.L116:
-	jmp .L137
-	.L140:
-	jmp .L139
-	.L139:
-	jmp .L138
-	.L138:
-	jmp .L115
-	.L137:
-	jmp .L142
-	.L142:
-	jmp .L136
-	.L141:
-	jmp .L136
-	.L136:
-	jmp .L143
-	.L143:
-	jmp .L135
-	.L135:
-	jmp .L134
-	.L144:
-	jmp .L134
-	.L134:
-	jmp .L145
-	.L145:
-	jmp .L6
-	.L115:
-	jmp .L147
-	.L147:
-	jmp .L6
-	.L146:
-	jmp .L114
-	.L114:
-	jmp .L150
-	.L150:
-	jmp .L113
-	.L149:
-	jmp .L113
-	.L148:
-	jmp .L151
-	.L151:
-	jmp .L6
-	.L113:
-	jmp .L153
-	.L153:
-	jmp .L112
-	.L152:
-	jmp .L154
-	.L154:
-	jmp .L112
-	.L112:
-	jmp .L6
-	.L156:
-	jmp .L6
-	.L155:
-	jmp .L111
-	.L111:
-	jmp .L159
-	.L161:
-	jmp .L160
-	.L160:
-	jmp .L159
-	.L159:
-	jmp .L158
-	.L162:
-	jmp .L158
-	.L158:
-	jmp .L157
-	.L157:
-	jmp .L6
-	.L6:
-	jmp .L168
-	.L169:
-	jmp .L166
-	.L168:
-	jmp .L166
-	.L167:
-	jmp .L166
-	.L166:
-	jmp .L165
-	.L170:
-	jmp .L5
-	.L165:
-	jmp .L178
-	.L178:
-	jmp .L164
-	.L177:
-	jmp .L176
-	.L179:
-	jmp .L164
-	.L176:
-	jmp .L180
-	.L180:
-	jmp .L175
-	.L175:
-	jmp .L164
-	.L181:
-	jmp .L174
-	.L174:
-	jmp .L164
-	.L173:
-	jmp .L172
-	.L172:
-	jmp .L171
-	.L171:
-	jmp .L5
-	.L164:
-	jmp .L183
-	.L187:
-	jmp .L186
-	.L186:
-	jmp .L183
-	.L185:
-	jmp .L183
-	.L184:
-	jmp .L183
-	.L183:
-	jmp .L188
-	.L189:
-	jmp .L182
-	.L188:
-	jmp .L182
-	.L182:
-	jmp .L190
-	.L190:
-	jmp .L5
-	.L163:
-	jmp .L198
-	.L198:
-	jmp .L193
-	.L197:
-	jmp .L199
-	.L199:
-	jmp .L193
-	.L196:
-	jmp .L201
-	.L201:
-	jmp .L195
-	.L200:
-	jmp .L195
-	.L195:
-	jmp .L204
-	.L204:
-	jmp .L203
-	.L203:
-	jmp .L194
-	.L202:
-	jmp .L193
-	.L194:
-	jmp .L193
-	.L193:
-	jmp .L211
-	.L211:
-	jmp .L0
-	.L210:
-	jmp .L209
-	.L209:
-	jmp .L208
-	.L208:
-	jmp .L0
-	.L207:
-	jmp .L0
-	.L206:
-	jmp .L0
-	.L205:
-	jmp .L213
-	.L213:
-	jmp .L212
-	.L212:
-	jmp .L192
-	.L214:
-	jmp .L0
-	.L192:
-	jmp .L216
-	.L219:
-	jmp .L216
-	.L218:
-	jmp .L216
-	.L217:
-	jmp .L216
-	.L216:
-	jmp .L220
-	.L222:
-	jmp .L220
-	.L221:
-	jmp .L220
-	.L220:
-	jmp .L215
-	.L223:
-	jmp .L215
-	.L215:
-	jmp .L227
-	.L228:
-	jmp .L227
-	.L227:
-	jmp .L0
-	.L226:
-	jmp .L225
-	.L225:
-	jmp .L229
-	.L229:
-	jmp .L224
-	.L224:
-	jmp .L191
-	.L191:
-	jmp .L235
-	.L235:
-	jmp .L233
-	.L234:
-	jmp .L232
-	.L236:
-	jmp .L233
-	.L233:
-	jmp .L237
-	.L237:
-	jmp .L5
-	.L232:
-	jmp .L238
-	.L238:
-	jmp .L5
-	.L231:
-	jmp .L5
-	.L230:
-	jmp .L5
-	.L239:
-	jmp .L0
-	.L5:
-	jmp .L242
-	.L248:
-	jmp .L247
-	.L247:
-	jmp .L242
-	.L246:
-	jmp .L242
-	.L245:
-	jmp .L242
-	.L244:
-	jmp .L242
-	.L243:
-	jmp .L241
-	.L249:
-	jmp .L242
-	.L242:
-	jmp .L255
-	.L256:
-	jmp .L250
-	.L255:
-	jmp .L250
-	.L254:
-	jmp .L253
-	.L253:
-	jmp .L250
-	.L252:
-	jmp .L251
-	.L251:
-	jmp .L250
-	.L250:
-	jmp .L259
-	.L260:
-	jmp .L241
-	.L259:
-	jmp .L258
-	.L258:
-	jmp .L241
-	.L257:
-	jmp .L0
-	.L261:
-	jmp .L241
-	.L241:
-	jmp .L264
-	.L267:
-	jmp .L266
-	.L266:
-	jmp .L264
-	.L265:
-	jmp .L264
-	.L264:
-	jmp .L240
-	.L263:
-	jmp .L240
-	.L262:
-	jmp .L268
-	.L268:
-	jmp .L240
-	.L240:
-	jmp .L273
-	.L274:
-	jmp .L273
-	.L273:
-	jmp .L275
-	.L275:
-	jmp .L272
-	.L272:
-	jmp .L4
-	.L276:
-	jmp .L4
-	.L271:
-	jmp .L277
-	.L278:
-	jmp .L270
-	.L277:
-	jmp .L279
-	.L279:
-	jmp .L270
-	.L270:
-	jmp .L269
-	.L280:
-	jmp .L4
-	.L269:
-	jmp .L0
-	.L4:
-	jmp .L283
-	.L283:
-	jmp .L3
-	.L284:
-	jmp .L3
-	.L282:
-	jmp .L285
-	.L286:
-	jmp .L285
-	.L285:
-	jmp .L281
-	.L287:
-	jmp .L3
-	.L281:
-	jmp .L0
-	.L291:
-	jmp .L290
-	.L290:
-	jmp .L0
-	.L292:
-	jmp .L0
-	.L289:
-	jmp .L295
-	.L298:
-	jmp .L297
-	.L297:
-	jmp .L296
-	.L296:
-	jmp .L288
-	.L295:
-	jmp .L299
-	.L299:
-	jmp .L294
-	.L294:
-	jmp .L288
-	.L293:
-	jmp .L288
-	.L288:
-	jmp .L303
-	.L303:
-	jmp .L302
-	.L302:
-	jmp .L3
-	.L304:
-	jmp .L301
-	.L301:
-	jmp .L305
-	.L305:
-	jmp .L300
-	.L300:
-	jmp .L306
-	.L306:
-	jmp .L0
-	.L3:
-	jmp .L308
-	.L308:
-	jmp .L309
-	.L309:
-	jmp .L307
-	.L307:
-	jmp .L311
-	.L311:
-	jmp .L310
-	.L310:
-	jmp .L0
+	subq $216, %rsp
+	pushq %rbp
+	movq %rsp, %rbp
+	movq %rdi, -8(%rbp)
+	movq %rsi, -16(%rbp)
 	.L0:
-	movq $42, -8(%rbp)
-	pushq %rdi
-	pushq %rax
-	movq -8(%rbp), %rdi
-	callq bx_print_int
-	popq %rax
-	popq %rdi
+	movq -8(%rbp), %r11
+	movq %r11, -24(%rbp)
+	movq -16(%rbp), %r11
+	movq %r11, -32(%rbp)
+	movq -24(%rbp), %r11
+	subq -32(%rbp), %r11
+	movq %r11, -24(%rbp)
+	movq $0, %r11
+	cmpq %r11, -24(%rbp)
+	jle .L1
 	jmp .L2
 	.L1:
-	movq $42, -16(%rbp)
-	movq -16(%rbp), %r11
-	negq %r11
-	movq %r11, -24(%rbp)
-	pushq %rdi
-	pushq %rax
-	movq -24(%rbp), %rdi
-	callq bx_print_int
-	popq %rax
-	popq %rdi
+	movq -8(%rbp), %r11
+	movq %r11, -40(%rbp)
+	movq $3, -48(%rbp)
+	movq -40(%rbp), %rax
+	cqto
+	idivq -48(%rbp)
+	movq %rdx, -56(%rbp)
+	movq $0, -64(%rbp)
+	movq -56(%rbp), %r11
+	subq -64(%rbp), %r11
+	movq %r11, -56(%rbp)
+	movq $0, %r11
+	cmpq %r11, -56(%rbp)
+	je .L3
+	jmp .L4
+	.L3:
+	movq -8(%rbp), %r11
+	movq %r11, -72(%rbp)
+	movq $5, -80(%rbp)
+	movq -72(%rbp), %rax
+	cqto
+	idivq -80(%rbp)
+	movq %rdx, -88(%rbp)
+	movq $0, -96(%rbp)
+	movq -88(%rbp), %r11
+	subq -96(%rbp), %r11
+	movq %r11, -88(%rbp)
+	movq $0, %r11
+	cmpq %r11, -88(%rbp)
+	je .L6
+	jmp .L7
+	.L6:
+	movq $151515, -104(%rbp)
+	movq -104(%rbp), %r11
+	movq %r11, -112(%rbp)
+	movq -120(%rbp), %rdi
+	callq __bx_print_int
+	addq $-39, %rsp
+	jmp .L8
+	.L7:
+	movq $333, -128(%rbp)
+	movq -128(%rbp), %r11
+	movq %r11, -112(%rbp)
+	movq -120(%rbp), %rdi
+	callq __bx_print_int
+	addq $-39, %rsp
+	.L8:
+	jmp .L5
+	.L4:
+	movq -8(%rbp), %r11
+	movq %r11, -136(%rbp)
+	movq $5, -144(%rbp)
+	movq -136(%rbp), %rax
+	cqto
+	idivq -144(%rbp)
+	movq %rdx, -152(%rbp)
+	movq $0, -160(%rbp)
+	movq -152(%rbp), %r11
+	subq -160(%rbp), %r11
+	movq %r11, -152(%rbp)
+	movq $0, %r11
+	cmpq %r11, -152(%rbp)
+	je .L9
+	jmp .L10
+	.L9:
+	movq $555, -168(%rbp)
+	movq -168(%rbp), %r11
+	movq %r11, -112(%rbp)
+	movq -120(%rbp), %rdi
+	callq __bx_print_int
+	addq $-39, %rsp
+	jmp .L11
+	.L10:
+	movq -8(%rbp), %r11
+	movq %r11, -176(%rbp)
+	movq -176(%rbp), %r11
+	movq %r11, -112(%rbp)
+	movq -120(%rbp), %rdi
+	callq __bx_print_int
+	addq $-39, %rsp
+	.L11:
+	.L5:
+	movq -8(%rbp), %r11
+	movq %r11, -184(%rbp)
+	movq $1, -192(%rbp)
+	movq -184(%rbp), %r11
+	addq -192(%rbp), %r11
+	movq %r11, -8(%rbp)
+	jmp .L0
 	.L2:
-	Lret:
+	movq x(%rip), %r11
+	movq %r11, -200(%rbp)
+	movq y(%rip), %r11
+	movq %r11, -208(%rbp)
+	movq -200(%rbp), %r11
+	addq -208(%rbp), %r11
+	movq %r11, y(%rip)
+	movq -8(%rbp), %r11
+	movq %r11, -216(%rbp)
+	movq -216(%rbp), %rax 
+	jmp .DopGM
+	.DopGM:
 	movq %rbp, %rsp
 	popq %rbp
-	xorq %rax, %rax
+	retq
+main :
+	pushq %rbp
+	movq %rsp, %rbp
+	subq $40, %rsp
+	pushq %rbp
+	movq %rsp, %rbp
+	movq $0, -8(%rbp)
+	movq -8(%rbp), %r11
+	movq %r11, -16(%rbp)
+	movq $100, -24(%rbp)
+	movq -24(%rbp), %r11
+	movq %r11, -16(%rbp)
+	movq -32(%rbp), %rdi
+	movq -32(%rbp), %rsi
+	callq fizzbuzz
+	addq $-32, %rsp
+	movq %rax, x
+	movq x(%rip), %r11
+	movq %r11, -40(%rbp)
+	movq -40(%rbp), %r11
+	movq %r11, -16(%rbp)
+	movq -32(%rbp), %rdi
+	callq __bx_print_int
+	addq $-39, %rsp
+	.SSBgQ:
+	movq %rbp, %rsp
+	popq %rbp
 	retq
--- labs/lab4/tac2x64.py
+++ labs/lab4/tac2x64.py
@@ -15,6 +15,8 @@ from ast2tac import Instr
 from typing import List
 import sys
 import os
+import random
+import string
 
 arg_nb_to_reg = {1:"rdi", 2:"rsi", 3:"rdx", 4:"rcx", 5:"r8", 6:"r9"}
 reg_to_arg_nb = {"rdi":1, "rsi":2, "rdx":3, "rcx":4, "r8":5, "r9":6}
@@ -72,28 +74,36 @@ unops = {'neg': 'negq',
 def lookup_temp(temp, temp_map):
     
     if temp[0] == "@" :
-        return f'({temp[1:]}%rip)'
+        print(f'{temp[1:]}(%rip)')
+        return f'{temp[1:]}(%rip)'
 
     assert (isinstance(temp, str) and
             temp[0] == '%'), temp
     return temp_map.setdefault(temp, f'{-8 * (len(temp_map) + 1)}(%rbp)')
 
 
-def tac_to_asm_proc(tac_instrs, args_proc):
+def tac_to_asm_proc(tac_instrs, args_proc,name_proc):
     """
     Get the x64 instructions correspondign to the TAC instructions for the procedure
     """
+    letters = string.ascii_letters  
     temp_map = dict()
     asm = []
+    ret_label = ''.join(random.choice(letters) for i in range(5)) 
+    reg_list = reg_to_arg_nb.keys()
+    for reg in reg_list :
+        asm.append(f'pushq %{reg}')
+    
+    asm.append(f'pushq $0')
+    
 
-    asm.append(f'pushq %rbp')
-    M = 1
+    M = len(reg_list)+1
     asm.append(f'movq %rsp, %rbp')
     
     for index_arg in range(0,len(args_proc)) :
         if index_arg<= 5 :
             stack_slot = lookup_temp(args_proc[index_arg],temp_map)
-            asm.append(f'movq {arg_nb_to_reg[index_arg+1]}, {stack_slot}')
+            asm.append(f'movq %{arg_nb_to_reg[index_arg+1]}, {stack_slot}')
         else :
             stack_slot = lookup_temp(args_proc[index_arg],temp_map)
             asm.append(f'movq {8*(index_arg+1)}(%rbp), {stack_slot}')
@@ -123,30 +133,32 @@ def tac_to_asm_proc(tac_instrs, args_proc):
             # empty the list of call arguments 
 
             for index_arg in range(1,min(6,args[1])+1) :
-                arg_temp = lookup_temp("%-"+index_arg, temp_map)
-                asm.append(f'movq {arg_temp}, {arg_nb_to_reg[index_arg]}')
+                arg_temp = lookup_temp('%-{index_arg}', temp_map)
+                asm.append(f'movq {arg_temp}, %{arg_nb_to_reg[index_arg]}')
             
             for index_arg in range(args[1], min(6,args[1]),-1) :
-                arg_temp = lookup_temp("%-"+index_arg, temp_map) 
+                arg_temp = lookup_temp('%-{index_arg}', temp_map) 
                 asm.append(f'pushq {arg_temp}')
             
             if args[1] > 6 and (args[1]%2) :
                 asm.append(f'pushq $0')
             
-            asm.append(f'callq {args[0]}')
+            asm.append(f'callq {args[0][1:]}')
             asm.append(f'addq ${  8  *  (args[1]-6) + args[1]%2 }, %rsp')
-            asm.append(f'movq %rax, {result[0]}')
+            if result :
+                
+                asm.append(f'movq %rax, {result[1:]}')
             
 
-        elif opcode == "retq" :
+        elif opcode == "ret" :
             if len(args) != 0 :
-                asm.append(f'movq {lookup_temp(args[0])}, %rax ')
-                asm.append(f'jmp Lret')
+                asm.append(f'movq {lookup_temp(args[0],temp_map)}, %rax ')
+                asm.append(f'jmp .{ret_label}')
 
 
             else :
                 asm.append(f'xorq %rax, %rax')
-                asm.append(f'jmp Lret')
+                asm.append(f'jmp .{ret_label}')
 
         elif opcode == 'const':
             assert len(args) == 1 and isinstance(args[0], int)
@@ -205,9 +217,13 @@ def tac_to_asm_proc(tac_instrs, args_proc):
     asm[:0] = [f'pushq %rbp',
                f'movq %rsp, %rbp',
                f'subq ${8 * len(temp_map)}, %rsp'] 
-    asm.extend([f'Lret:',
-                f'movq %rbp, %rsp',
-                f'popq %rbp',
+    asm.extend([f'.{ret_label}:',
+                f'movq %rbp, %rsp'])
+    asm.append(f'addq $8, %rsp')
+    for reg in reg_list[::-1] :
+        asm.append(f'popq %{reg}')
+
+    asm.extend([f'popq %rbp',
                 f'retq'])
     return asm
 
@@ -245,12 +261,12 @@ def compile_tac_from_json(fname):
     
     for json_obj in tjs :
         if "proc" in json_obj.keys() :
-            declarations.append(f'\t.globl {json_obj["proc"]}')
-            asm += [f'{json_obj["proc"]} :']
-            asm += ['\t' + line for line in tac_to_asm_proc(json_obj['body'],json_obj["args"])]
+            declarations.append(f'\t.globl {json_obj["proc"][1:]}')
+            asm += [f'{json_obj["proc"][1:]} :']
+            asm += ['\t' + line for line in tac_to_asm_proc(json_obj['body'],json_obj["args"],json_obj["proc"][1:])]
         if "var" in json_obj.keys() :
-            declarations.append(f'\t.globl {json_obj["var"]}')
-            data.append(f'{json_obj["var"]}:  .quad {json_obj["init"]}')
+            declarations.append(f'\t.globl {json_obj["var"][1:]}')
+            data.append(f'{json_obj["var"][1:]}:  .quad {json_obj["init"]}')
 
     
     asm[:0] = declarations + [f'.data'] + data + [f'.text']
