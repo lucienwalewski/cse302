2316549b699cfaff1a1bc034e06ad60814509959
Lucien <lucien.walewski@polytechnique.edu>
2021-11-07 11:08:35 +0100

    Updated bug for bx_runtime.c

--- labs/lab4/bx_runtime.c
+++ labs/lab4/bx_runtime.c
@@ -1,5 +1,5 @@
 #include <stdint.h>
 #include <stdio.h>
 
-void __bx_print_int(int64_t x) { printf("%lld\n", x); }
+void __bx_print_int(int64_t x) { printf("%ld\n", x); }
 void __bx_print_bool(int64_t b) { printf("%s\n", b ? "true" : "false"); }
