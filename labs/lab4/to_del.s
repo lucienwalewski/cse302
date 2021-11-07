8cb4c068a02c920d12ff14ff6996eedebb6b41f7
Alban Puech <alban.puech@truite.polytechnique.fr>
2021-11-07 18:13:42 +0100

    added new tests

---
+++ labs/lab4/to_del.s
@@ -0,0 +1,75 @@
+	.globl x
+	.globl y
+	.globl function
+	.globl z
+	.globl main
+.data
+x:  .quad 10
+y:  .quad 15
+z:  .quad 1
+.text
+function :
+	pushq %rbp
+	movq %rsp, %rbp
+	subq $64, %rsp
+	movq %rdi, -72(%rbp)
+	.L3:
+	movq x(%rip), %r11
+	movq %r11, -80(%rbp)
+	movq -80(%rbp), %r11
+	movq %r11, -88(%rbp)
+	movq -88(%rbp), %rdi
+	callq __bx_print_int
+	movq $20, -96(%rbp)
+	movq -96(%rbp), %r11
+	movq %r11, -104(%rbp)
+	movq -104(%rbp), %r11
+	movq %r11, -88(%rbp)
+	movq -88(%rbp), %rdi
+	callq __bx_print_int
+	jmp .L0
+	.L0:
+	movq $30, -112(%rbp)
+	movq -112(%rbp), %r11
+	movq %r11, -120(%rbp)
+	movq -120(%rbp), %r11
+	movq %r11, -88(%rbp)
+	movq -88(%rbp), %rdi
+	callq __bx_print_int
+	jmp .L2
+	.L1:
+	.L2:
+	movq -96(%rbp), %r11
+	movq %r11, -128(%rbp)
+	movq -128(%rbp), %r11
+	movq %r11, -88(%rbp)
+	movq -88(%rbp), %rdi
+	callq __bx_print_int
+	xorq %rax, %rax
+	jmp .IkCOK
+	.IkCOK:
+	movq %rbp, %rsp
+	popq %rbp
+	retq
+main :
+	pushq %rbp
+	movq %rsp, %rbp
+	subq $32, %rsp
+	.L1:
+	movq $5, -72(%rbp)
+	movq -72(%rbp), %r11
+	movq %r11, -80(%rbp)
+	movq -80(%rbp), %rdi
+	callq function
+	movq x(%rip), %r11
+	movq %r11, -88(%rbp)
+	movq -88(%rbp), %r11
+	movq %r11, -80(%rbp)
+	movq -80(%rbp), %rdi
+	callq __bx_print_int
+	xorq %rax, %rax
+	jmp .WCDJd
+	.WCDJd:
+	movq %rbp, %rsp
+	popq %rbp
+	retq
